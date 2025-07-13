d1aef18765429154a2052e88c917dfd2e2593642
harsha101195 <st3364@columbia.edu>
2025-07-13 03:33:18 -0700

    adding comments

--- assignment_2/qr_generator.py
+++ assignment_2/qr_generator.py
@@ -2,17 +2,23 @@ import qrcode
 from PIL import Image
 
 def generate_qr(url, filename="qrcode.png"):
+    # Create a qrcode object
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
         box_size=10,
         border=4
     )
+
+    # Provide data
     qr.add_data(url)
     qr.make(fit=True)
+
+    # Generate QR code image
     img = qr.make_image(fill_color="black", back_color="white")
     img.save(filename)
 
+    # Open the image
     img = Image.open(filename)
     img.show()
 
